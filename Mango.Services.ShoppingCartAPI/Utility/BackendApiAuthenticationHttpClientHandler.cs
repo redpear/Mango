43f54f4660af9176ccec00c7d671882d471ac4c8
Angie <redpear@mac.com>
2023-08-23 17:36:42 -0700

    working code update

---
+++ Mango.Services.ShoppingCartAPI/Utility/BackendApiAuthenticationHttpClientHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Utility
+{
+    public class BackendApiAuthenticationHttpClientHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _accessor;
+
+        public BackendApiAuthenticationHttpClientHandler(IHttpContextAccessor accessor)
+        {
+          _accessor = accessor;
+        }
+
+
+    }
+}
